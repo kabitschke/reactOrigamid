2b7a3435bd4c61ee17542bccca8d00ae0d161d4f
Maycon Kabitschke <mayconkabitschke@gmail.com>
2026-08-06 17:46:27 -0300

    components form

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from 'react';
-import { CheckBox } from './components/CheckBox';
-
-
 
 const App = () => {
-
-
-
-  return (
-    <div>
-      <h1>Vite</h1>
-      <CheckBox />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
