3fa42ed35ec22875e06d43dd014c37a2d35625d1
maycon <mayconkabitschke@gmail.com>
2026-08-17 06:03:38 -0300

    api produto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Produto from './components/Produto';
+
+
 
 
 const App = () => {
 
   return (
     <div>
+      <Produto />
 
     </div>
   );
